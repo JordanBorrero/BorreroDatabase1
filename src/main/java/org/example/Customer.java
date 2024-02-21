7d16c23ef77827e137dc62372ebe13829ce39184
jordanborrero <jmb9380@psu.edu>
2024-02-20 20:44:41 -0500

    first commit

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Customer {
+    private String Customerid;
+    private String Customername;
+    private String Customerage;
+    private String Customeraddress;
+
+    public String getCustomerid() {
+        return Customerid;
+    }
+
+    public void setCustomerid(String customerid) {
+        Customerid = customerid;
+    }
+
+    public String getCustomername() {
+        return Customername;
+    }
+
+    public void setCustomername(String customername) {
+        Customername = customername;
+    }
+
+    public String getCustomerage() {
+        return Customerage;
+    }
+
+    public void setCustomerage(String customerage) {
+        Customerage = customerage;
+    }
+
+    public String getCustomeraddress() {
+        return Customeraddress;
+    }
+
+    public void setCustomeraddress(String customeraddress) {
+        Customeraddress = customeraddress;
+    }
+}
